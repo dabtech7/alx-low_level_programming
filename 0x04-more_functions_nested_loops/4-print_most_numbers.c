0995473f386e8791b6d53f61c1439dd46eb2aedc
dabtech <bismarkdwamenaakenteng@gmail.com>
2023-10-13 02:02:14 +0000

    task 4 of 0x04-more_functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_most_numbers - print numbers to stdo
+ */
+
+void print_most_numbers(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		if (c == '2' || c == '4')
+		{
+			continue;
+		}
+		else
+		{
+			_putchar(c);
+		}
+	}
+	_putchar('\n');
+}

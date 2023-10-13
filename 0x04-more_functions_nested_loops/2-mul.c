b38aa92ce165838c7db61ea502edd2d50035032f
dabtech <bismarkdwamenaakenteng@gmail.com>
2023-10-13 01:54:32 +0000

    task 2 of 0x04-more_functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * mul - multiply two numbers
+ * @a: first integer param
+ * @b: second integer param
+ * Return: the results of mul
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}

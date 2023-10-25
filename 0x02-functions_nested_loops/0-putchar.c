e8b937a4e2973b0e2791691c55f53dd36903a204
dabtech <bismarkdwamenaakenteng@gmail.com>
2023-10-25 03:08:03 +0000

    add fuction to print putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: prints _putchar using prototype _putchar
+ *
+ * Return: Always return 0 (success)
+ */
+
+int main(void)
+{
+	char str[]="_putchar";
+	int i;
+
+	for(i=0; i<8; i++)
+		_putchar(str[i]);
+	_putchar('\n');
+	
+	return (0);
+}

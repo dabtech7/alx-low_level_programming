df028c7f07de8cf22cd8566f8aac84ab3b34a7f7
dabtech <bismarkdwamenaakenteng@gmail.com>
2023-11-09 01:41:40 +0000

    adds task 2 file

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,69 @@
+/**
+ * op_add - adds @a and @b
+ *
+ * @a: input 1
+ * @b: input 2
+ *
+ * Return: sum of @a and @b
+*/
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+
+/**
+ * op_sub - retrieves difference of @a and @b
+ *
+ * @a: input 1
+ * @b: input 2
+ *
+ * Return: difference of @a and @b
+*/
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+
+/**
+ * op_mul - multiply @a and @b
+ *
+ * @a: input 1
+ * @b: input 2
+ *
+ * Return: product on @a and @b
+*/
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+
+/**
+ * op_div - divides @a by @b
+ *
+ * @a: input 1
+ * @b: input 2
+ *
+ * Return: results of the division @a and @b
+*/
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+
+/**
+ * op_mod - recieves reminder of the division between @a and @b
+ *
+ * @a: input 1
+ * @b: input 2
+ *
+ * Return: the remainder
+*/
+int op_mod(int a, int b)
+{
+	return (a % b);
+}

5d107ba8d8cd407d05eee8c14905cae1c28aafc1
dabtech7 <bismarkdwamenaakenteng@gmail.com>
2023-11-22 05:33:42 +0000

    task 1

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * listint_len - a returns the number of elements.
+ *
+ * @h: pointer to the first node
+ *
+ * Return: numb of elements
+*/
+size_t listint_len(const listint_t *h)
+{
+	size_t elements = 1;
+
+	if (h == NULL)
+		return (0);
+
+	while (h->next != NULL)
+	{
+		if (h->n != '\0')
+			elements++;
+
+		h = h->next;
+	}
+
+	return (elements);
+}

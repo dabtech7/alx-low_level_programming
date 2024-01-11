c5364edde41d653a104b8d01ac9a20dab3deefe9
dabtech <bismarkdwamenaakenteng@gmail.com>
2024-01-11 08:17:13 +0000

    adds task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,13 @@
-#include <stdio.h>
 #include "main.h"
+
 /**
- * reset_to_98 - reset a value of addresst o 98
- * @n: pointer to reset its value
+ * reset_to_98 - takes a pointer to a parameter and updates its value.
+ * @n: input integer.
+ * Return: no return.
  */
 
 void reset_to_98(int *n)
 {
 	*n = 98;
 }
+
